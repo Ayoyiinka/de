db0fa027908e7f07971e5550d7ffb108e06dd3d4
Ayoyinka Obisesan <ayoyinkaobisesan@192.168.1.189>
2025-01-06 22:46:07 +0000

    add config structure

---
+++ config.py
@@ -0,0 +1,16 @@
+config = {
+    "google_api_key": "...",
+    "youtube_playlist_id": "...",
+    "page_token": "...",
+    "kafka": {
+        "bootstrap.servers": "...",
+        "security.protocol": "...",
+        "sasl.mechanism": "...",
+        "sasl.username": "...",
+        "sasl.password": "...",
+    },
+    "schema_registry": {
+        "url": "...",
+        "basic.auth.user.info": "...",
+    }
+}
